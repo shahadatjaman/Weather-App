427f6f393bedd95cc0fb4ccad7c3b5927c5b428e
Shahadat Jaman <devloperabuhuraira@gmail.com>
2023-06-24 18:20:41 +0600

    Single forecast or item

--- src/Components/NextDays__Forecast/index.tsx
+++ src/Components/NextDays__Forecast/index.tsx
@@ -1,15 +1,30 @@
+import React from "react";
 import H2 from "../../Shared/Heading/H2";
 import Item from "./Item";
 import "./Styles.css";
+import { useLocation } from "react-router-dom";
+import { useGetForecastQuery } from "../../features/weather/weatherApi";
 
 const NextForecast = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const state = searchParams.get("state");
+  const { data: forecast } = useGetForecastQuery({ q: state, days: 7 });
+
   return (
     <div className="next_forecast_wrapper">
       <H2 transform="capitalize">7-day forecast</H2>
 
       <div>
-        {[1, 1, 1, 1, 1, 1, 1].map((item, index) => (
-          <Item />
+        {forecast?.forecast?.forecastday?.map((item: any, index: number) => (
+          <React.Fragment key={index}>
+            <Item
+              icon={item.day?.condition?.icon}
+              text={item.day?.condition?.text}
+              forecast={item}
+            />
+          </React.Fragment>
         ))}
       </div>
     </div>
