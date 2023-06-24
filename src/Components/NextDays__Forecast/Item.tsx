e8ed4fbc6268bf2b9f97aa656742924873810618
Shahadat Jaman <devloperabuhuraira@gmail.com>
2023-06-24 18:19:35 +0600

    Next 7 day's forecast component

--- src/Components/NextDays__Forecast/Item.tsx
+++ src/Components/NextDays__Forecast/Item.tsx
@@ -1,19 +1,43 @@
-import { icons } from "./data";
+import { FC } from "react";
 
-const Item = () => {
+import { useDispatch } from "react-redux";
+import { addForecast } from "../../features/weather/weatherSlice";
+import getDayName from "../../Utils/getDayName";
+import { getCurrentDayName } from "../../Utils/getCurrentDayName";
+
+interface PropsTypes {
+  icon: string;
+  text: string;
+  forecast: any;
+}
+
+const Item: FC<PropsTypes> = ({ icon, text, forecast }) => {
+  const dispatch = useDispatch();
+
+  const addWeatherHandler = () => {
+    dispatch(addForecast(forecast));
+  };
+
   return (
-    <div className="py-6 forecast_item_wrapper flex justify-between">
-      <div>Today</div>
+    <div
+      onClick={addWeatherHandler}
+      className="py-6 forecast_item_wrapper flex justify-between"
+    >
+      <div>
+        {getDayName(forecast.date) === getCurrentDayName()
+          ? "Today's"
+          : getDayName(forecast.date)}
+      </div>
       <div className="flex justify-center items-center">
         <div>
-          <img src={icons.sun} alt="sunny" width={30} />
+          <img src={icon} alt="sunny" width={30} />
         </div>
-        <h6 className="font-bold ml-3 text-sm">Sunny</h6>
+        <h6 className="font-bold ml-3 text-sm">{text}</h6>
       </div>
-      <div>
+      {/* <div>
         <span className="font-bold">95</span>
         /55
-      </div>
+      </div> */}
     </div>
   );
 };
